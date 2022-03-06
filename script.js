a8c30695a404c85b33e1506d8cca8ef5619fa6c2
Assad Isah <nottherealalanturing@gmail.com>
2022-03-06 22:32:33 +0100

    feat: deletes book when you click on delete icon

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const shelf = document.querySelector("#shelf");
-const modal = document.querySelector(".modal");
-const modalBtn = document.querySelector(".add-btn");
-const close = document.getElementsByClassName("close-modal")[0];
-const addbookbtn = document.getElementById("submit");
+const shelf = document.querySelector('#shelf');
+const modal = document.querySelector('.modal');
+const modalBtn = document.querySelector('.add-btn');
+const close = document.getElementsByClassName('close-modal')[0];
+const addbookbtn = document.getElementById('submit');
+let deleten = document.querySelector(`[data-title="${this.title}"`);
 
 const myLibrary = new Library();
 
@@ -20,21 +21,21 @@ Library.prototype.myLibrary = [];
 
 Library.prototype.addBookToLibrary = function (e) {
   e.preventDefault();
-  const author = document.getElementById("title").value;
-  const title = document.getElementById("author").value;
-  const pages = document.getElementById("pages").value;
-  const img = document.getElementById("images");
-  const image = img.value.slice(img.value.lastIndexOf("\\") + 1) || "book.jpg";
-  const isRead = document.getElementById("checkbox").value;
+  const author = document.getElementById('title').value;
+  const title = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const img = document.getElementById('images');
+  const image = img.value.slice(img.value.lastIndexOf('\\') + 1) || 'book.jpg';
+  const isRead = document.getElementById('checkbox').value;
   this.myLibrary.push(new Book(author, title, pages, image, isRead));
   myLibrary.displayBooks();
   closeModal();
 };
 
 Library.prototype.displayBooks = function () {
-  shelf.innerHTML = "";
+  shelf.innerHTML = '';
   if (this.myLibrary.length === 0) {
-    shelf.innerHTML += "Library is empty";
+    shelf.innerHTML += 'Library is empty';
   } else {
     for (let i = 0; i < this.myLibrary.length; i++) {
       const book = `
@@ -47,19 +48,28 @@ Library.prototype.displayBooks = function () {
             </div>
           </div>
           <div class="book-deets-2 mt-10">
-          <a href="#" >
+          <a href="#">
               <img src="./assets/icons/check.svg" alt="read" />
            </a>
-           <a href="#" id="deleteBtn">
+           <a href="#" id="deleteBtn-${i}">
               <img src="./assets/icons/delete.svg" alt="delete" />
            </a>
           <p>${this.myLibrary[i].pageCount} pages</p>
           </div>
         </article>
     `;
-      const deleteBtn = document.querySelector(`[data-title="${this.title}"`);
-
       shelf.innerHTML += book;
+    }
+
+    if (this.myLibrary.length !== 0) {
+      for (let i = 0; i < this.myLibrary.length; i++) {
+        document
+          .getElementById(`deleteBtn-${i}`)
+          .addEventListener('click', (e) => {
+            deleteBookFromLibrary(i);
+            this.displayBooks();
+          });
+      }
     }
   }
 };
@@ -72,29 +82,28 @@ Library.prototype.deleteBook = function (title) {
 };
 
 function closeModal() {
-  modal.style.display = "none";
+  modal.style.display = 'none';
 }
 
-modalBtn.addEventListener("click", () => {
-  modal.style.display = "block";
+modalBtn.addEventListener('click', () => {
+  modal.style.display = 'block';
 });
 
-close.addEventListener("click", () => {
+close.addEventListener('click', () => {
   closeModal();
 });
 
-addbookbtn.addEventListener("click", (e) => {
+addbookbtn.addEventListener('click', (e) => {
   myLibrary.addBookToLibrary(e);
 });
 
-deleteBtn.addEventListener("click", (e) => {
-  console.log(e);
-  /* myLibrary.deleteBook(); */
-});
+function deleteBookFromLibrary(item) {
+  myLibrary.myLibrary.splice(item, 1);
+}
 
-window.addEventListener("click", (e) => {
+window.addEventListener('click', (e) => {
   if (e.target === modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 });
 
